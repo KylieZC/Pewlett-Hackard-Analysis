057796b784207ed1a37a8ddb58163c179ff35c81
ZhiyiChen <92655991+KylieZC@users.noreply.github.com>
2021-12-08 23:15:55 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -6,24 +6,25 @@ CREATE TABLE departments(
  	UNIQUE (dept_name)
 );
 
--- Create table for employees
+-- Create table for PH-Employees
 CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY(emp_no)
 );
 
---Create data_manaer table
+--Create table for PH-Employees
 CREATE TABLE dept_manager(
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -37,5 +38,47 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+-- Create titles table
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, title,from_date),
+	FOREIGN KEY (emp_no) REFERENCES salaries,
+	FOREIGN KEY (emp_no) REFERENCES employees
+);
+
+-- Create Deparment employment table 
+
+DROP TABLE dept_emp
+CASCADE
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees,
+	FOREIGN KEY (dept_no) REFERENCES departments,
+	FOREIGN KEY (emp_no) REFERENCES salaries
+);
+
 SELECT *
-FROM departments;
+FROM departments
+
+SELECT *
+FROM employees
+
+SELECT *
+FROM dept_manager
+
+SELECT *
+FROM salaries
+
+SELECT *
+FROM titles
+
+SELECT *
+FROM dept_emp
